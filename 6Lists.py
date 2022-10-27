434e825a49572bc5b966a9ebe9e2e39b641e30df
chiyochan <chiyochan9@gmail.com>
2022-10-26 19:08:54 -0700

    custom sort function

--- 6Lists.py
+++ 6Lists.py
@@ -23,6 +23,14 @@ numLstDest = [345, 3000, 1, 2300]
 numLstDest.sort(reverse = True)
 print("Sort descending ", numLstDest)
 
+# Custom sort function
+def myfunc(n):
+  return abs(n - 50)
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(key = myfunc)
+print(thislist)
+
 # Reverse  list
 thislist = ["banana", "Orange", "Kiwi", "cherry"]
 thislist.reverse()
